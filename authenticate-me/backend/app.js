6a92365a753b47c1060b3ffe0bd073c7862458e6
Huishi <anhuishi95@gmail.com>
2022-10-19 16:38:19 -0700

    Add error-handling middleware for common errors and debugging

--- authenticate-me/backend/app.js
+++ authenticate-me/backend/app.js
@@ -10,6 +10,9 @@ const { environment } = require('./config');
 // Check if the environment is in production
 const isProduction = environment === 'production';
 
+// Sequelize Error-Handler
+const { ValidationError } = require('sequelize');
+
 const app = express();
 
 // Connect the morgan middleware for logging information about requests and responses
@@ -46,5 +49,36 @@ const routes = require('./routes');
 // Connect all the routes
 app.use(routes);
 
+// Catch unhandled requests and forward to error handler.
+app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = ["The requested resource couldn't be found."];
+    err.status = 404;
+    next(err);
+});
+
+
+// Process sequelize errors
+app.use((err, _req, _res, next) => {
+    // check if error is a Sequelize error:
+    if (err instanceof ValidationError) {
+        err.errors = err.errors.map((e) => e.message);
+        err.title = 'Validation error';
+    }
+    next(err);
+});
+
+// Error formatter
+app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+      title: err.title || 'Server Error',
+      message: err.message,
+      errors: err.errors,
+      stack: isProduction ? null : err.stack
+    });
+  });
 
 module.exports = app;
--- authenticate-me/backend/routes/index.js
+++ authenticate-me/backend/routes/index.js
@@ -3,11 +3,15 @@ const router = express.Router();
 
 // Add a XSRF-TOKEN cookie
 router.get("/api/csrf/restore", (req, res) => {
-    const csrfToken = req.csrfToken();
-    res.cookie("XSRF-TOKEN", csrfToken);
-    res.status(200).json({
-      'XSRF-Token': csrfToken
-    });
+  const csrfToken = req.csrfToken();
+  res.cookie("XSRF-TOKEN", csrfToken);
+  res.status(200).json({
+    'XSRF-Token': csrfToken
   });
+});
+
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
 
 module.exports = router;
