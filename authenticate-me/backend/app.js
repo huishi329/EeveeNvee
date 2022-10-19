eb7b931c9725ab5e472a51cecd95206dc6a4b584
Huishi <anhuishi95@gmail.com>
2022-10-19 13:41:40 -0700

    express setup

---
+++ authenticate-me/backend/app.js
@@ -0,0 +1,50 @@
+const express = require('express');
+require('express-async-errors');
+const morgan = require('morgan');
+const cors = require('cors');
+const csurf = require('csurf');
+const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
+
+const { environment } = require('./config');
+// Check if the environment is in production
+const isProduction = environment === 'production';
+
+const app = express();
+
+// Connect the morgan middleware for logging information about requests and responses
+app.use(morgan('dev'));
+
+app.use(cookieParser());
+app.use(express.json());
+
+// Security Middleware
+if (!isProduction) {
+    // enable cors only in development
+    app.use(cors());
+}
+
+// helmet helps set a variety of headers to better secure your app
+app.use(
+    helmet.crossOriginResourcePolicy({
+        policy: "cross-origin"
+    })
+);
+
+// Set the _csrf token and create req.csrfToken method
+app.use(
+    csurf({
+        cookie: {
+            secure: isProduction,
+            sameSite: isProduction && "Lax",
+            httpOnly: true
+        }
+    })
+);
+
+const routes = require('./routes');
+// Connect all the routes
+app.use(routes);
+
+
+module.exports = app;
