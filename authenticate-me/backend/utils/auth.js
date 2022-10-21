6d6a6908f00655637db875b94069e592b56ec2b2
Huishi <anhuishi95@gmail.com>
2022-10-20 20:59:24 -0700

    Add User authentication Express middleware

---
+++ authenticate-me/backend/utils/auth.js
@@ -0,0 +1,66 @@
+const jwt = require('jsonwebtoken');
+const { jwtConfig } = require('../config');
+const { User } = require('../db/models');
+
+const { secret, expiresIn } = jwtConfig;
+
+// middlewares
+const setTokenCookie = (res, user) => {
+    // Create the token.
+    const token = jwt.sign(
+        { data: user.toSafeObject() },
+        secret,
+        { expiresIn: parseInt(expiresIn) } // 604,800 seconds = 1 week
+    );
+
+    const isProduction = process.env.NODE_ENV === "production";
+
+    // Set the token cookie
+    res.cookie('token', token, {
+        maxAge: expiresIn * 1000, // maxAge in milliseconds
+        httpOnly: true,
+        secure: isProduction,
+        sameSite: isProduction && "Lax"
+    });
+
+    return token;
+};
+
+// restore the session user based on the contents of the JWT cookie.
+const restoreUser = (req, res, next) => {
+    // token parsed from cookies
+    const { token } = req.cookies;
+    req.user = null;
+
+    return jwt.verify(token, secret, null, async (err, jwtPayload) => {
+        if (err) {
+            return next();
+        }
+
+        try {
+            const { id } = jwtPayload.data;
+            req.user = await User.scope('currentUser').findByPk(id);
+        } catch (e) {
+            res.clearCookie('token');
+            return next();
+        }
+        // user not found
+        if (!req.user) res.clearCookie('token');
+
+        return next();
+    });
+};
+
+// Require a session user to be authenticated before accessing a route
+// If there is no current user, return an error
+const requireAuth = function (req, _res, next) {
+    if (req.user) return next();
+
+    const err = new Error('Unauthorized');
+    err.title = 'Unauthorized';
+    err.errors = ['Unauthorized'];
+    err.status = 401;
+    return next(err);
+}
+
+module.exports = { setTokenCookie, restoreUser, requireAuth };
