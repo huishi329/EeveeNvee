9b7726244fa079d8bf442016db4b21db964b227a
Huishi <anhuishi95@gmail.com>
2022-10-24 10:38:40 -0700

    Add Review model, migration and seed data

---
+++ backend/db/seeders/20221024171419-demo-review.js
@@ -0,0 +1,50 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+     await queryInterface.bulkInsert('Reviews', [
+      {
+        spotId: 3,
+        userId: 1,
+        review: 'Gorgeous peaceful setting, hosts quick to reply and answer any questions. Accommodation so cute and well thought out, nice to have kayaks and SUPs available. Definitely will be back.',
+        stars: 5
+      },
+      {
+        spotId: 3,
+        userId: 4,
+        review: 'It was everything we were looking for - beautiful, peaceful, and clean. The private sauna was delightful and the view from the backyard, picturesque.',
+        stars: 5
+      },
+      {
+        spotId: 1,
+        userId: 5,
+        review: 'Easy to get to and centrally located. The apartment is clean and on a quiet floor which made relaxing easy. A great place to stay!',
+        stars: 4
+      },
+     ], {})
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+     await queryInterface.bulkDelete('Reviews', {
+      where: {id: {
+        [Op.in]: [1, 2, 3]
+      }}
+     })
+  }
+};
