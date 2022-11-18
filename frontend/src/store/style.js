222e7feaefa6adb3a826f90846c6914a5f798dc8
Huishi <anhuishi95@gmail.com>
2022-11-18 09:19:41 -0800

    Adapt to 1024px window

--- frontend/src/store/style.js
+++ frontend/src/store/style.js
@@ -2,8 +2,8 @@ const NARROW_NAVBAR = 'style/NARROW_NARBAR';
 const RESTORE_NAVBAR = 'style_RESTORE_NAVBAR';
 
 const narrowNavbar = {
-    header: { width: '80rem' },
-    menu: { right: '13vw' }
+    // header: { width: '80rem' },
+    // menu: { right: '0vw' }
 };
 
 export const setNavbar = () => {
