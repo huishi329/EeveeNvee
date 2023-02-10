a40eae6b7be87533506f51c5b7ab956a6f285635
Huishi <anhuishi95@gmail.com>
2023-02-09 17:57:53 -0800

    Change className

--- frontend/src/components/SpotDetail/SpotImages/SpotImages.js
+++ frontend/src/components/SpotDetail/SpotImages/SpotImages.js
@@ -5,9 +5,9 @@ export default function SpotImages({ images }) {
         <div className={styles.wrapper}>
             <img className={styles.img1} src={images["0"].url} alt='spot image'></img>
             {images["1"] && <img className={styles.img2} src={images["1"]?.url} alt='spot image'></img>}
-            {images["2"] && <img className={styles.img2} src={images["2"]?.url} alt='spot image'></img>}
-            {images["3"] && <img className={styles.img2} src={images["3"]?.url} alt='spot image'></img>}
-            {images["4"] && <img className={styles.img2} src={images["4"]?.url} alt='spot image'></img>}
+            {images["2"] && <img className={styles.img3} src={images["2"]?.url} alt='spot image'></img>}
+            {images["3"] && <img className={styles.img4} src={images["3"]?.url} alt='spot image'></img>}
+            {images["4"] && <img className={styles.img5} src={images["4"]?.url} alt='spot image'></img>}
         </div >
     )
 }
