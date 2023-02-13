46da1f4dad94975fdd24ab051f9e74e3c1e8e5fe
Huishi <anhuishi95@gmail.com>
2023-02-13 13:37:24 -0800

    Add ImageDropdownMenu

---
+++ frontend/src/components/DragAndDropImage/ImageEditor/ImageDropdownMenu/ImageDropdownMenu.js
@@ -0,0 +1,20 @@
+import styles from './ImageDropdownMenu.module.css';
+
+export default function ImageDropdownMenu({ position }) {
+    return (
+        <div className={styles.dropdownMenu}>
+            <div className={styles.menuItem}>
+                <span>Move forward</span>
+            </div>
+            <div className={styles.menuItem}>
+                <span>Move backward</span>
+            </div>
+            <div className={styles.menuItem}>
+                <span>Make cover photo</span>
+            </div>
+            <div className={styles.menuItem}>
+                <span>Delete</span>
+            </div>
+        </div>
+    )
+}
