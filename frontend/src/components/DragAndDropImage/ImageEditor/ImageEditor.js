1cde6475e1e9ac0c21bf743208f4cf480b32f164
Huishi <anhuishi95@gmail.com>
2023-02-13 13:37:53 -0800

    Add ImageDropdownMenu

--- frontend/src/components/DragAndDropImage/ImageEditor/ImageEditor.js
+++ frontend/src/components/DragAndDropImage/ImageEditor/ImageEditor.js
@@ -1,21 +1,22 @@
 import styles from './ImageEditor.module.css';
 import { useState } from 'react';
+import ImageDropdownMenu from './ImageDropdownMenu/ImageDropdownMenu';
 
 export default function ImageEditor({ imgURL, position }) {
     const [showDropDownMenu, setShowDropDownMenu] = useState(false);
     return (
-        <div >
+        <div draggable>
             <div className={styles.previewImage}>
                 <img className="spotImage" src={imgURL} alt={`${position}`} />
                 {position === 0 && <div className={styles.cover}>Cover photo</div>}
-                <button type='button' className={styles.button} onClick={setShowDropDownMenu(true)}>
+                <button type='button' className={styles.button}
+                    onClick={() => setShowDropDownMenu(true)}
+                >
                     <i className="fa-solid fa-ellipsis"></i>
                 </button>
             </div>
             {showDropDownMenu &&
-                <div>
-
-                </div>}
+                <ImageDropdownMenu />}
         </div>
     )
 }
