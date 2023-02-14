712c80176c3499c16aa277be06880549fd6a1c2c
Huishi <anhuishi95@gmail.com>
2023-02-13 20:28:45 -0800

    Add event listeners

--- frontend/src/components/DragAndDropImage/ImageEditor/ImageEditor.js
+++ frontend/src/components/DragAndDropImage/ImageEditor/ImageEditor.js
@@ -1,11 +1,31 @@
 import styles from './ImageEditor.module.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import ImageDropdownMenu from './ImageDropdownMenu/ImageDropdownMenu';
+
 
 export default function ImageEditor({ imgURL, position }) {
     const [showDropDownMenu, setShowDropDownMenu] = useState(false);
+
+    useEffect(() => {
+        if (!showDropDownMenu) return;
+
+        const closeDropdownMenu = () => {
+            setShowDropDownMenu(false);
+        };
+        // click anywhere on the window, close the profile menu
+        const modal = document.getElementById('editSpotForm');
+        console.log(modal);
+        modal.addEventListener('click', closeDropdownMenu);
+        modal.addEventListener('scroll', closeDropdownMenu);
+
+        return () => {
+            modal.removeEventListener("click", closeDropdownMenu);
+            modal.removeEventListener("scroll", closeDropdownMenu);
+        }
+    }, [showDropDownMenu]);
+
     return (
-        <div draggable>
+        <div className={styles.wrapper} draggable>
             <div className={styles.previewImage}>
                 <img className="spotImage" src={imgURL} alt={`${position}`} />
                 {position === 0 && <div className={styles.cover}>Cover photo</div>}
