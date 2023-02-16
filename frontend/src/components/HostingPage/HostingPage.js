ee7413890435ece1d5fb27942a5abea796b76098
Huishi <anhuishi95@gmail.com>
2023-02-15 18:52:48 -0800

    Display hostingSpots with HostingItem

--- frontend/src/components/HostingPage/HostingPage.js
+++ frontend/src/components/HostingPage/HostingPage.js
@@ -2,8 +2,8 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { getHostingSpots } from '../../store/spot';
+import HostingList from './HostingList/HostingList';
 import styles from './HostingPage.module.css';
-
 
 export default function HostingPage() {
     const user = useSelector(state => state.session.user);
@@ -21,10 +21,17 @@ export default function HostingPage() {
     if (!hostingSpots) return null;
 
     return (
-        <div>
-            <h1>
-                {Object.keys(hostingSpots).length} listings
-            </h1>
+        <div className={styles.wrapper}>
+            <div className={styles.top}>
+                <h2>
+                    {Object.keys(hostingSpots).length} listings
+                </h2>
+                <button className={styles.button}>
+                    <i className="fa-solid fa-plus"></i>
+                    Create listing
+                </button>
+            </div>
+            <HostingList hostingSpots={hostingSpots} />
         </div>
     )
 };
