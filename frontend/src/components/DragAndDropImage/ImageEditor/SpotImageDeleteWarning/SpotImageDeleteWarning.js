633f69e8c28078e83a3d1b717b5abd9b1fac0142
Huishi <anhuishi95@gmail.com>
2023-02-17 11:00:57 -0800

    Add SpotImageDeleteWarning

---
+++ frontend/src/components/DragAndDropImage/ImageEditor/SpotImageDeleteWarning/SpotImageDeleteWarning.js
@@ -0,0 +1,37 @@
+import { useDispatch } from "react-redux";
+import { deleteSpotImage } from "../../../../store/spot";
+import styles from "./SpotImageDeleteWarning.module.css";
+
+export default function SpotImageDeleteWarning({ image, setShowSpotImageDeleteWarning }) {
+    const dispatch = useDispatch();
+    const handleDelete = () => {
+        dispatch(deleteSpotImage(image.id, image.position))
+            .then(() => setShowSpotImageDeleteWarning(false));
+    };
+    console.log('HEYYYYY');
+
+    return (
+        <div className={styles.wrapper}>
+            <div className={styles.warning}>
+                <div className={styles.title}>
+                    <i className="fa-solid fa-xmark" onClick={() => setShowSpotImageDeleteWarning(false)}></i>
+                    <div className={styles.titleText}>Delete this photo?</div>
+                    <div className={styles.placeholder}></div>
+                </div>
+
+                <div className={styles.message}>Once you delete it, this action cannot be undone.</div>
+                <div className={styles.buttons}>
+                    <div className={styles.cancel}
+                        onClick={() => setShowSpotImageDeleteWarning(false)}
+                    >
+                        Cancel
+                    </div>
+                    <div className={styles.delete}
+                        onClick={handleDelete}>
+                        Delete it
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
