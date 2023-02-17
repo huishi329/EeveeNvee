8a6d3961fa1b803f2b3075d3745447bdf8a7a3c0
Huishi <anhuishi95@gmail.com>
2023-02-17 14:46:53 -0800

    Add patch spot-images route

--- backend/routes/api/spot-images.js
+++ backend/routes/api/spot-images.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router();
 const { SpotImage } = require('../../db/models');
-const { singleMulterUpload, singlePublicFileUpload } = require('../../aws')
+const { Op } = require('sequelize');
 const { restoreUser, requireAuth } = require('../../utils/auth');
 
 
@@ -33,9 +33,46 @@ router.delete('/:imageId', restoreUser, requireAuth, isImageExisting,
 
         await spotImage.destroy();
 
+        const followingSpotImages = await SpotImage.findAll({
+            where: {
+                spotId: {
+                    [Op.is]: spot.id,
+                },
+                position: {
+                    [Op.gt]: spotImage.position
+                }
+            }
+        });
+
+        followingSpotImages.forEach(async (spotImage) => {
+            spotImage.set({ position: spotImage.position - 1 });
+            await spotImage.save();
+        });
+
+
         res.json({
             message: "Successfully deleted",
-            statusCode: 200
+            statusCode: 200,
+        });
+    }
+)
+
+router.patch('/:imageId', restoreUser, requireAuth, isImageExisting,
+    async (req, res) => {
+        const { spotImage, user } = req;
+        const spot = await spotImage.getSpot();
+        if (spot.ownerId !== user.id) {
+            return res.status(403).json({
+                message: "Forbidden",
+                statusCode: 403
+            });
+        };
+        spotImage.set({ position: req.body.position });
+        await spotImage.save();
+        res.json({
+            id: spotImage.id,
+            url: spotImage.url,
+            position: spotImage.position,
         });
     }
 )
