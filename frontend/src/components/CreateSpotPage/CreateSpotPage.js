2466ef8d736d979573c8b299739b8b874fbb0fbe
Huishi <anhuishi95@gmail.com>
2023-02-17 09:55:48 -0800

    Add CreateSpotPage

---
+++ frontend/src/components/CreateSpotPage/CreateSpotPage.js
@@ -0,0 +1,20 @@
+import { useNavigate } from 'react-router-dom';
+import EditSpotForm from '../EditSpotForm/EditSpotForm';
+import styles from './CreateSpotPage.module.css';
+
+
+export default function CreateSpotPage({ }) {
+    const navigate = useNavigate();
+
+    return (
+        <div className={styles.wrapper}>
+            <div className={styles.title}>
+                <i className="fa-solid fa-xmark" onClick={() => navigate('/listings')}></i>
+                <h1>Create a listing</h1>
+                <div></div>
+            </div>
+
+            <EditSpotForm />
+        </div>
+    );
+}
