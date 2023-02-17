9f95131f4ed72be51e6bfba24b6d50295d408a78
Huishi <anhuishi95@gmail.com>
2023-02-16 16:28:13 -0800

    Handle delete

--- frontend/src/components/DragAndDropImage/ImageEditor/ImageDropdownMenu/ImageDropdownMenu.js
+++ frontend/src/components/DragAndDropImage/ImageEditor/ImageDropdownMenu/ImageDropdownMenu.js
@@ -1,6 +1,15 @@
 import styles from './ImageDropdownMenu.module.css';
+import { useDispatch } from 'react-redux';
+import { deleteSpotImage } from '../../../../store/spot';
 
-export default function ImageDropdownMenu({ position }) {
+
+export default function ImageDropdownMenu({ image }) {
+    const dispatch = useDispatch();
+
+    const handleDelete = () => {
+        dispatch(deleteSpotImage(image.id, image.position));
+    };
+
     return (
         <div className={styles.dropdownMenu}>
             <div className={styles.menuItem}>
@@ -12,7 +21,8 @@ export default function ImageDropdownMenu({ position }) {
             <div className={styles.menuItem}>
                 <span>Make cover photo</span>
             </div>
-            <div className={styles.menuItem}>
+            <div className={styles.menuItem}
+                onClick={handleDelete}>
                 <span>Delete</span>
             </div>
         </div>
