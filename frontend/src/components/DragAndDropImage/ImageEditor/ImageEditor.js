90172de8e789c2f78d7bbb3e57e4f057abf3ae1d
Huishi <anhuishi95@gmail.com>
2023-02-17 13:29:17 -0800

    Make drag and drop to rearrange

--- frontend/src/components/DragAndDropImage/ImageEditor/ImageEditor.js
+++ frontend/src/components/DragAndDropImage/ImageEditor/ImageEditor.js
@@ -3,10 +3,23 @@ import { useEffect, useState } from 'react';
 import ImageDropdownMenu from './ImageDropdownMenu/ImageDropdownMenu';
 import { Modal } from '../../../context/Modal';
 import SpotImageDeleteWarning from './SpotImageDeleteWarning/SpotImageDeleteWarning';
+import { useDispatch } from 'react-redux';
+import { shuffleSpotImages } from '../../../store/spot';
 
-export default function ImageEditor({ image }) {
+export default function ImageEditor({ image, dragStartImage, setDragStartImage }) {
+    const dispatch = useDispatch();
     const [showDropDownMenu, setShowDropDownMenu] = useState(false);
     const [showSpotImageDeleteWarning, setShowSpotImageDeleteWarning] = useState(false);
+    const handleDrop = (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        dispatch(shuffleSpotImages(dragStartImage, image));
+    };
+
+    const handleDefault = (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+    }
 
 
     useEffect(() => {
@@ -25,8 +38,15 @@ export default function ImageEditor({ image }) {
         }
     }, [showDropDownMenu]);
 
+
     return (
-        <div className={styles.wrapper} draggable>
+        <div className={styles.wrapper}
+            draggable
+            onDragStart={() => setDragStartImage(image)}
+            onDrop={handleDrop}
+            onDragOver={handleDefault}
+            onDragEnter={handleDefault}
+        >
             <div className={styles.previewImage}>
                 <img className="spotImage" src={image.url} alt={`${image.position}`} />
                 {image.position === 0 && <div className={styles.cover}>Cover photo</div>}
