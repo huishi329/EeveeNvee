a0e9a87654dd85c32ab834844a353b7ffa962f25
Huishi <anhuishi95@gmail.com>
2023-02-18 11:17:02 -0800

    Add TripPage

---
+++ frontend/src/components/TripPage/TripPage.js
@@ -0,0 +1,10 @@
+import styles from './TripPage.module.css';
+
+export default function TripPage() {
+    return (
+        <div className={styles.wrapper}>
+            <h1 className={styles.header}>Trips</h1>
+        </div>
+    )
+
+}
