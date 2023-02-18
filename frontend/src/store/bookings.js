64947b554ddea3afbe789caffae34379fe4745a3
Huishi <anhuishi95@gmail.com>
2023-02-18 12:36:44 -0800

    Add getCurrentBooking thunk action

--- frontend/src/store/bookings.js
+++ frontend/src/store/bookings.js
@@ -2,7 +2,11 @@ import { csrfFetch } from "./crsf";
 
 const GET_CURRENT_BOOKINGS = 'bookings/GET_CURRENT_BOOKINGS';
 
-const getCurrentBookings = (bookings) => async dispatch => {
+export const getCurrentBookings = (bookings) => async dispatch => {
+    const response = await csrfFetch('/api/bookings/current', {
+        method: 'GET',
+    });
+    const bookings = await response.json();
     dispatch({ type: GET_CURRENT_BOOKINGS, bookings });
 };
 
@@ -16,7 +20,7 @@ const bookingReducer = (state = initialState, action) => {
     const newState = { ...state };
     switch (action.type) {
         case GET_CURRENT_BOOKINGS:
-            newState.trips = action.bookings.trips;
+            newState.trips = action.bookings;
             return newState;
         default:
             return state;
