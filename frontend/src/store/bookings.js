5c9272f5e88051df6687796a414eec56ad027db9
Huishi <anhuishi95@gmail.com>
2023-02-18 14:00:01 -0800

    Sort and filter bookings

--- frontend/src/store/bookings.js
+++ frontend/src/store/bookings.js
@@ -7,7 +7,13 @@ export const getCurrentBookings = (bookings) => async dispatch => {
         method: 'GET',
     });
     const bookings = await response.json();
-    dispatch({ type: GET_CURRENT_BOOKINGS, bookings });
+    const currentDate = new Date();
+    currentDate.setHours(23, 59, 59, 59);
+    const upcomingTrips = bookings.filter(booking => new Date(booking.endDate) >= currentDate);
+    upcomingTrips.sort((a, b) => new Date(a.startDate) - new Date(b.startDate));
+    const pastTrips = bookings.filter(booking => new Date(booking.endDate) < currentDate);
+    pastTrips.sort((a, b) => new Date(b.startDate) - new Date(a.startDate));
+    dispatch({ type: GET_CURRENT_BOOKINGS, upcomingTrips, pastTrips });
 };
 
 
@@ -20,7 +26,8 @@ const bookingReducer = (state = initialState, action) => {
     const newState = { ...state };
     switch (action.type) {
         case GET_CURRENT_BOOKINGS:
-            newState.trips = action.bookings;
+            const { upcomingTrips, pastTrips } = action;
+            newState.trips = { upcomingTrips, pastTrips };
             return newState;
         default:
             return state;
