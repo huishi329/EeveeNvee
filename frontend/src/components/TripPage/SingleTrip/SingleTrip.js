702975053c5e420e20a0d6274e1ba4154e083aad
Huishi <anhuishi95@gmail.com>
2023-02-19 22:00:06 -0800

    Make two modals linked to SingleTrip

--- frontend/src/components/TripPage/SingleTrip/SingleTrip.js
+++ frontend/src/components/TripPage/SingleTrip/SingleTrip.js
@@ -3,22 +3,34 @@ import { useLocation } from 'react-router-dom';
 import { Modal } from '../../../context/Modal';
 import ReservationConfirmation from '../../ReservationConfirmation/ReservationConfirmation';
 import styles from './SingleTrip.module.css';
+import TripCancelWarning from './TripCancelWarning/TripCancelWarning';
 
 export default function SingleTrip({ booking }) {
     let { startDate, endDate } = booking;
     const location = useLocation();
     const [showReservation, setShowReservation] = useState(false);
+    const [showTripCancelWarning, setShowTripCancelWarning] = useState(false);
     const { title, street, city, state, country } = booking.Spot;
     startDate = new Date(startDate);
     endDate = new Date(endDate);
+
+    const showReservationModal = (e) => {
+        setShowTripCancelWarning(false);
+        setShowReservation(true);
+    }
+
+    const showTripCancelWarningModal = (e) => {
+        setShowReservation(false);
+        setShowTripCancelWarning(true);
+    }
 
     return (
         <div className={styles.wrapper}>
             <div className={styles.infoWrapper}>
                 <h2 className={styles.title}
-                    onClick={() => setShowReservation(true)}>{title}</h2>
+                    onClick={showReservationModal}>{title}</h2>
                 <div className={styles.details}
-                    onClick={() => setShowReservation(true)}>
+                    onClick={showReservationModal}>
                     <div className={styles.date}>
                         <div className={styles.day}>
                             <div>
@@ -47,7 +59,7 @@ export default function SingleTrip({ booking }) {
                 <div className={styles.buttonDiv}>
                     {location.pathname.includes('past') ?
                         <button>Rate my stay</button> :
-                        <button>Cancel trip</button>}
+                        <button onClick={showTripCancelWarningModal}>Cancel trip</button>}
                 </div>
             </div>
             <div className={styles.imageWrapper}
@@ -58,6 +70,12 @@ export default function SingleTrip({ booking }) {
                 showReservation &&
                 <Modal onClose={() => setShowReservation(false)}>
                     <ReservationConfirmation reservation={booking} spot={booking.Spot} setShowReservation={setShowReservation} />
+                </Modal>
+            }
+            {
+                showTripCancelWarning &&
+                <Modal onClose={() => setShowTripCancelWarning(false)}>
+                    <TripCancelWarning bookingId={booking.id} setShowTripCancelWarning={setShowTripCancelWarning} />
                 </Modal>
             }
         </div >
