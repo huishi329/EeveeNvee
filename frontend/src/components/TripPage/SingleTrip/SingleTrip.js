c891849b7ea03f8bfc2834b777f6601d5e4e465a
Huishi <anhuishi95@gmail.com>
2023-02-19 19:55:15 -0800

    Add ReservationConfirmation Modal

--- frontend/src/components/TripPage/SingleTrip/SingleTrip.js
+++ frontend/src/components/TripPage/SingleTrip/SingleTrip.js
@@ -1,9 +1,13 @@
+import { useState } from 'react';
 import { useLocation } from 'react-router-dom';
+import { Modal } from '../../../context/Modal';
+import ReservationConfirmation from '../../ReservationConfirmation/ReservationConfirmation';
 import styles from './SingleTrip.module.css';
 
 export default function SingleTrip({ booking }) {
     let { startDate, endDate } = booking;
     const location = useLocation();
+    const [showReservation, setShowReservation] = useState(false);
     const { title, street, city, state, country } = booking.Spot;
     startDate = new Date(startDate);
     endDate = new Date(endDate);
@@ -11,8 +15,10 @@ export default function SingleTrip({ booking }) {
     return (
         <div className={styles.wrapper}>
             <div className={styles.infoWrapper}>
-                <h2 className={styles.title}>{title}</h2>
-                <div className={styles.details}>
+                <h2 className={styles.title}
+                    onClick={() => setShowReservation(true)}>{title}</h2>
+                <div className={styles.details}
+                    onClick={() => setShowReservation(true)}>
                     <div className={styles.date}>
                         <div className={styles.day}>
                             <div>
@@ -44,9 +50,16 @@ export default function SingleTrip({ booking }) {
                         <button>Cancel trip</button>}
                 </div>
             </div>
-            <div className={styles.imageWrapper}>
+            <div className={styles.imageWrapper}
+                onClick={() => setShowReservation(true)}>
                 <img src={booking.Spot.previewImage} alt='spot' className={styles.image} />
             </div>
+            {
+                showReservation &&
+                <Modal onClose={() => setShowReservation(false)}>
+                    <ReservationConfirmation reservation={booking} spot={booking.Spot} setShowReservation={setShowReservation} />
+                </Modal>
+            }
         </div >
     )
 }
