10a9537c7b19256c4a26161d9152740129b9c0a1
Huishi <anhuishi95@gmail.com>
2023-02-19 17:21:09 -0800

    Add getSpotBookings thunk action

--- frontend/src/store/bookings.js
+++ frontend/src/store/bookings.js
@@ -2,8 +2,10 @@ import { csrfFetch } from "./crsf";
 
 const GET_CURRENT_BOOKINGS = 'bookings/GET_CURRENT_BOOKINGS';
 const CREATE_BOOKING = 'bookings/CREATE_BOOKING';
+const DELETE_BOOKING = 'bookings/DELETE_BOOKING';
+const GET_SPOT_BOOKINGS = 'bookings/GET_SPOT_BOOKINGS';
 
-export const getCurrentBookings = (bookings) => async dispatch => {
+export const getCurrentBookings = () => async dispatch => {
     const response = await csrfFetch('/api/bookings/current', {
         method: 'GET',
     });
@@ -17,6 +19,14 @@ export const getCurrentBookings = (bookings) => async dispatch => {
     dispatch({ type: GET_CURRENT_BOOKINGS, upcomingTrips, pastTrips });
 };
 
+export const getSpotBookings = (spotId) => async dispatch => {
+    const response = await csrfFetch(`/api/spots/${spotId}/bookings`, {
+        method: 'GET',
+    });
+    const bookings = await response.json();
+    dispatch({ type: GET_SPOT_BOOKINGS, bookings });
+};
+
 export const createBooking = (spotId, booking) => async dispatch => {
     const response = await csrfFetch(`/api/spots/${spotId}/bookings`, {
         method: 'POST',
@@ -27,10 +37,19 @@ export const createBooking = (spotId, booking) => async dispatch => {
     dispatch({ type: CREATE_BOOKING, newBooking });
 };
 
+export const deleteBooking = (bookingId) => async dispatch => {
+    await csrfFetch(`/api/bookings/${bookingId}`, {
+        method: 'DELETE',
+    });
+    dispatch({ type: DELETE_BOOKING, bookingId });
+};
+
+
 
 const initialState = {
     trips: null,
     hosting: null,
+    spot: null
 };
 
 const bookingReducer = (state = initialState, action) => {
@@ -40,10 +59,18 @@ const bookingReducer = (state = initialState, action) => {
             const { upcomingTrips, pastTrips } = action;
             newState.trips = { upcomingTrips, pastTrips };
             return newState;
+        case GET_SPOT_BOOKINGS:
+            newState.spot = { ...action.bookings };
+            return newState;
         case CREATE_BOOKING:
             newState.trips = { ...newState.trips };
             newState.trips.upcomingTrips = [...newState.trips.upcomingTrips, action.newBooking];
             newState.trips.upcomingTrips.sort((a, b) => new Date(a.startDate) - new Date(b.startDate));
+            return newState;
+        case DELETE_BOOKING:
+            newState.trips = { ...newState.trips };
+            newState.trips.upcomingTrips = { ...newState.trips.upcomingTrips };
+            delete newState.trips.upcomingTrips[action.bookingId];
             return newState;
         default:
             return state;
